8d47609d17dd885ff2d09e537cfa057fe528ea8b
Matheus Vieira <rochavieira.matheus@gmail.com>
2026-01-10 02:28:11 -0300

    fix: CreateNotificationUseCase

--- src/modules/notifications/application/use-cases/create-notification.use-case.ts
+++ src/modules/notifications/application/use-cases/create-notification.use-case.ts
@@ -6,7 +6,6 @@ import {
 import { AppLogger } from '../../../../shared/logger/app-logger.service';
 import { Notification } from '../../domain/entities/notification.entity';
 import { NotificationStatus } from '../../domain/enums/notification-status.enum';
-import { NotificationType } from '../../domain/enums/notification-type.enum';
 import { NotificationRepository } from '../../domain/repositories/notification.repository';
 import { CreateNotificationStrategy } from '../strategies/create-notification.strategy';
 import { ProcessNotificationUseCase } from './process-notification.use-case';
@@ -22,13 +21,11 @@ export class CreateNotificationUseCase {
 
   async execute(input: any): Promise<Notification> {
     try {
-      const message = input.type === NotificationType.BUTTON_PIX ? '' : input.message;
-
       const notification = await this.notificationRepository.create(
         Notification.create({
           type: input.type,
           to: input.to,
-          message,
+          message: input.message,
           status: NotificationStatus.QUEUED,
         }),
       );
@@ -38,8 +35,7 @@ export class CreateNotificationUseCase {
 
       await this.processNotification.execute(payload);
 
-      const updated = await this.notificationRepository.findById(notification.id);
-      return updated ?? notification;
+      return notification;
     } catch (err) {
       this.logger.error(err);
       if (err instanceof HttpException) throw err;
